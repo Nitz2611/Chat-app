449a8dff9a90831dc1b29685ed59a3320dea2b4a
Nitish sharma <sharmanitish183@gmail.com>
2024-01-13 13:11:07 +0530

    added comments to understand

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,45 @@
-const express = require("express");
-const cors = require("cors");
-const { default: axios } = require("axios");
+const express = require("express"); // importing express module
+const cors = require("cors"); // importing cors module
+const { default: axios } = require("axios"); // importing axios module
 
+// creating an instance of express
 const app = express();
+
+// using middleware to parse json request bodies
 app.use(express.json());
+
+// using cors middleware to allow cross-origin requests
 app.use(cors({ origin: true }));
 
+// defining a route for user authentication
 app.post("/authenticate", async (req, res) => {
+  // destructuring the username from the request body
   const { username } = req.body;
-  try{
 
+  try {
+    // making a put request to the chatengine api to create a new user
     const r = await axios.put(
-        'https://api.chatengine.io/users/',
-        {username: username, secret: username, first_name: username},
-        {headers: {"private-key": "2f42e79e-9a25-4407-a09d-af2d9da14f31"}}
-    )
-        return res.status(r.status).json(r.data)
-  }catch(e){
-    return res.status(e.response.status).json(e.response.data)
+      // the endpoint for creating a new user
+      'https://api.chatengine.io/users/',
+      // the new user data
+      {
+        username: username,
+        secret: username,
+        first_name: username
+      },
+      // setting the private-key header with the chatengine api key
+      {
+        headers: { "private-key": "2f42e79e-9a25-4407-a09d-af2d9da14f31" }
+      }
+    );
+
+    // returning the response from the chatengine api
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    // if there was an error, return the error response from the chatengine api
+    return res.status(e.response.status).json(e.response.data);
   }
-  
-  
 });
 
+// starting the express server on port 3001
 app.listen(3001);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,30 @@
-import { useState } from "react";
+import { useState } fromreact";
 
 import "./App.css";
 
+// Import the AuthPage component for displaying the login/signup page
 import AuthPage from "./AuthPage";
+
+// Import the ChatsPage component foring the chats page
 import ChatsPage from "./ChatsPage";
 
 function App() {
+  // Use the useState hook to create a state variable called "user"
+  // and a function to update it called "setUser"
   const [user, setUser] = useState(undefined);
 
+  // If the user is not logged in (i.e. the "user" state is undefined),
+  // render the AuthPage component and pass the "setUser" function
+  // as a prop called "onAuth"
   if (!user) {
     return <AuthPage onAuth={(user) => setUser(user)} />;
-  } else {
+  }
+  // If the user is logged in (i.e. the "user" state is not undefined),
+  // render the ChatsPage component and pass the "user" object as a prop
+  else {
     return <ChatsPage user={user} />;
   }
 }
 
+// Export the App component for use in other modules
 export default App;
